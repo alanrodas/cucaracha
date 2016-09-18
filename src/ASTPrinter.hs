e2c9e3941afb81cc83e9fb4aa259a196d8635f80
Alan Rodas Bonjour <alanrodas@gmail.com>
2016-09-18 14:33:33 -0300

    Added pretty printing

--- src/ASTPrinter.hs
+++ src/ASTPrinter.hs
@@ -1,36 +1,101 @@
-module ASTPrinter(stringify) where
+module ASTPrinter where
 import Parser
+import Text.PrettyPrint
 
-stringify :: ProgramT -> String
-stringify _  = ""
--- stringify EmptyProgram    = ""
--- stringify (Program funcs) = "(Program\n" ++ strMany 1 funcs ++ "\n)"
+idnt = 1
 
-{-}
-nodeRepr :: Integer -> a -> [String]
-nodeRepr n x = case x of
-    Function ident typ param block -> ["Function", ident, show typ]--, strMany n param, strOne n block]
-    Parameter ident typ -> ["Parameter"]
-    _ -> ["Otro"]
+instance Show ProgramT where
+  show p = render (programRepr p)
 
 
+programRepr EmptyProgram = empty
+programRepr (Program funcs) =
+  parentesis (
+    text      "Program"           $$
+    listRepr  funcRepr funcs idnt
+  )
 
-indent :: Integer -> String
-indent 0 = ""
-indent n = "  " ++ indent (n-1)
+funcRepr (Function name typ params block) n =
+  nestify n "Function" [nested name, typeRepr typ, listRepr paramRepr params, blockRepr block]
 
-strMany :: Integer -> [a] -> String
-strMany n [] = ""
-strMany n (x:xs) = strOne n x ++ strMany n xs
+typeRepr typ n = nested (show typ) n
 
-strOne :: Integer -> a -> String
-strOne n x = indent n ++ "(" ++ strElem n x ++ "\n" ++ indent n ++ ")"
+paramRepr (Parameter name typ) n =
+  nestify n "Parameter" [nested name, typeRepr typ]
 
-strElem :: Integer -> a -> String
-strElem n e = strAll n (nodeRepr n e)
+blockRepr (Block stmts) n =
+  nestify n "Block" [listRepr stmtRepr stmts]
 
-strAll :: Integer -> [String] -> String
-strAll n ([]) = ""
-strAll n (x:[]) = x
-strAll n (x:xs) = x ++ "\n" ++ indent (n+1) ++ strAll n xs
--}
+stmtRepr (StmtAssign name expr) n =
+  nestify n "StmtAssign" [nested name, exprRepr expr]
+stmtRepr (StmtVecAssign name exprL exprR) n =
+  nestify n "StmtVecAssign" [nested name, exprRepr exprL, exprRepr exprR]
+stmtRepr (StmtIf expr block) n =
+  nestify n "StmtIf" [exprRepr expr, blockRepr block]
+stmtRepr (StmtIfElse expr blockIf blockElse) n =
+  nestify n "StmtIfElse" [exprRepr expr, blockRepr blockIf, blockRepr blockElse]
+stmtRepr (StmtWhile expr block) n =
+  nestify n "StmtWhile" [exprRepr expr, blockRepr block]
+stmtRepr (StmtReturn expr) n =
+  nestify n "StmtReturn" [exprRepr expr]
+stmtRepr (StmtCall name exprsns) n =
+  nestify n "StmtCall" [nested name, listRepr exprRepr exprsns]
+
+exprRepr (ExprVar name) n =
+  nestify n "ExprVar" [nested name]
+exprRepr (ExprConstNum val) n =
+  nestify n "ExprConstNum" [nested (show val)]
+exprRepr (ExprConstBool val) n =
+  nestify n "ExprConstBool" [nested (show val)]
+exprRepr (ExprVecMake exprsns) n =
+  nestify n "ExprVecMake" [listRepr exprRepr exprsns]
+exprRepr (ExprVecLength name) n =
+  nestify n "ExprVecLength" [nested name]
+exprRepr (ExprVecDeref name expr) n =
+  nestify n "ExprVecDeref" [nested name, exprRepr expr]
+exprRepr (ExprCall name exprsns) n =
+  nestify n "ExprCall" [nested name, listRepr exprRepr exprsns]
+exprRepr (ExprNot expr) n =
+  nestify n "ExprNot" [exprRepr expr]
+exprRepr (ExprAnd exprL exprR) n =
+  nestify n "ExprAnd" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprOr exprL exprR) n =
+  nestify n "ExprOr" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprLe exprL exprR) n =
+  nestify n "ExprLe" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprGe exprL exprR) n =
+  nestify n "ExprGe" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprLt exprL exprR) n =
+  nestify n "ExprLt" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprGt exprL exprR) n =
+  nestify n "ExprGt" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprEq exprL exprR) n =
+  nestify n "ExprEq" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprNe exprL exprR) n =
+  nestify n "ExprNe" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprAdd exprL exprR) n =
+  nestify n "ExprAdd" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprSub exprL exprR) n =
+  nestify n "ExprSub" [exprRepr exprL, exprRepr exprR]
+exprRepr (ExprMul exprL exprR) n =
+  nestify n "ExprMul" [exprRepr exprL, exprRepr exprR]
+
+
+nested :: String -> Int -> Doc
+-- Return a doc of the given string nested a given amount of places to the right
+nested str n = nest n (text str)
+
+parentesis :: Doc -> Doc
+-- Return the document wrapped in parentesis with the expected newlines added
+parentesis x = lparen <> x $$ rparen
+
+nestedParens :: Int -> Doc -> Doc
+-- Return the document indented by a given space and wrapped around the expected parens
+nestedParens n x = nest n (parentesis x)
+
+listRepr :: (a -> Int -> Doc) -> [a] -> Int ->  Doc
+-- Get the doc representation of a list of elements of any type
+listRepr f ls n = foldl ($$) empty (map (\x -> f x n) ls)
+
+nestify :: Int -> String -> [(Int -> Doc)] -> Doc
+nestify n name elems = nestedParens n (foldl (\a e -> a $$ (e n) ) (text name) elems)
